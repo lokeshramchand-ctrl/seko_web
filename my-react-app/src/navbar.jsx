f6641539e52bd8f95627329f268ba6ff845e76bd
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-04 04:39:53 +0530

    439

---
+++ my-react-app/src/navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+       <nav className = "navbar">
+        <h1>Seko</h1>
+        <ul>
+            <li><a href = "/">Home</a></li>
+            <li><a href = "/">About</a></li>
+            <li><a href = "/">Services</a></li>
+        </ul>
+        </nav>
+        );
+
+}
+
+export default Navbar;
