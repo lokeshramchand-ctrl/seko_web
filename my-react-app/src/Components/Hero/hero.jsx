6f4ea36b6982af73e442d8c51358293f53642843
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-05 01:17:51 +0530

    01.07

--- my-react-app/src/Components/Hero/hero.jsx
+++ my-react-app/src/Components/Hero/hero.jsx
@@ -2,11 +2,16 @@ import './hero.css';
 
 const Hero = () => {
     return (
-        <div class ="hero">
-           <img src = "src/assets/4.jpg"/>
+        <div className="hero">
+            <img src="src/assets/4.jpg" />
+            <div className="text">
+                <h1>Empowering Farmers Through Direct Market Access</h1>
+            </div>
+            <div class = "hero-button">
+    <a href="#">Coming soon</a>
+    </div>
         </div>
         );
-
 }
 
 export default Hero;
--- my-react-app/src/Components/Navbar/navbar.jsx
+++ my-react-app/src/Components/Navbar/navbar.jsx
@@ -3,11 +3,13 @@ import './navbar.css';
 const Navbar = () => {
     return (
 <nav class="navbar">
-    <div class="logo">MyLogo</div>
+    <div class="logo">
+        <img src ='src/assets/3.png'></img>
+    </div>
     <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Timeline</a></li>
         <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
     </ul>
     <div class = "contact-button">
     <a href="#">Contact</a>
