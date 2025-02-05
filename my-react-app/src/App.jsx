150d1a4db18e5d18115f8076e6b619e765002bf3
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-05 09:01:46 +0530

    0901

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar/navbar.jsx';
 import Hero from './Components/Hero/hero.jsx';
-
+import Footer from './Components/Footer/footer.jsx';
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Footer/>
     </div>
   );
 }
---
+++ my-react-app/src/Components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <div class ="">
+            </div>
+            </div>
+        );
+}
+
+export default Footer;
