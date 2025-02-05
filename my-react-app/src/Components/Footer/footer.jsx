d7747cd4935b47a30028b4d060503edf80141ca4
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-05 11:13:13 +0530

    1113

--- my-react-app/src/Components/Footer/footer.jsx
+++ my-react-app/src/Components/Footer/footer.jsx
@@ -2,10 +2,37 @@ import './footer.css';
 
 const Footer = () => {
     return (
-        <div class="footer">
-            <div class ="">
-            </div>
-            </div>
+        <div class = "footer-container">
+            <div class = "footer-logo">
+                <img src = 'src/assets/3.png'></img>
+                </div>
+            <div class = "footer-email">
+                <h1>Request Early Access</h1>
+                <form>
+                    <input type = "email" placeholder = "Email Address"></input>
+                    <button>Connect</button>
+                </form>
+        </div>
+        <div class = "footer-about">
+            <div class = "footer-merge">
+        <div class = "footer-pages">
+            <h1>Quick Links</h1>
+            <a href = "#">About Us</a>
+            <a href = "#">Contact Us</a>
+            <a href = "#">Blog</a>
+            <a href = "#">Privacy Policy</a>
+            <a href = "#">Terms</a>
+        </div>
+        <div class = "footer-connect">
+            <h1>Connect</h1>
+            <a href = "#">Facebook</a>
+            <a href = "#">Twitter</a>
+            <a href = "#">Instagram</a>
+            <a href = "#">LinkedIn</a>
+        </div>
+        </div>
+        </div>
+        </div>
         );
 }
 
