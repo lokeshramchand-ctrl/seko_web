b0223c930cd5025aebadb30699ffd9848b6fb11c
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-07 08:50:18 +0530

    08.50

---
+++ my-react-app/src/Components/About/about.jsx
@@ -0,0 +1,19 @@
+import './about.css';
+
+const About = () => {
+    return (
+        <div class = "about">
+            <h1>About us</h1>
+            <p>Our mission</p>
+            
+            <div class = "about-image">
+            <img src = 'src/assets/1.png'></img>
+        </div>
+        
+        </div>
+        
+
+        );
+}
+
+export default About;
