ffe26a64947ead896b007c1fb21d1f8a46585eac
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-07 10:38:44 +0530

    1030

--- my-react-app/src/Components/About/about.jsx
+++ my-react-app/src/Components/About/about.jsx
@@ -2,17 +2,18 @@ import './about.css';
 
 const About = () => {
     return (
-        <div class = "about">
+        <div class="about">
             <h1>About us</h1>
-            <p>Our mission</p>
-            <div class = "about-border">
-            <div class = "about-image">
-            <img src = 'src/assets/6.jpg'></img>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div class="about-border">
+                <div class="about-image">
+                    <img src='src/assets/6.jpg'></img>
+                </div>
             </div>
         </div>
-        </div>
 
-        );
+    );
 }
 
 export default About;
